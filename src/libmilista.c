2d516f4630d7fe48faf677d2539a9d77a8fa6535
marcosan <marcoxavibsc@gmail.com>
2017-06-22 17:58:11 -0500

    [UPD] Lista_Inicializar

--- src/libmilista.c
+++ src/libmilista.c
@@ -1,7 +1,11 @@
 #include "miLista.h"
+#include <stdlib.h>
 /*Definicion de funciones sobre la lista*/
 
 extern int Lista_Inicializar(ListaEnlazada *lista){
+	lista->ancla.siguiente = NULL;
+	lista->ancla.anterior = NULL;
+	lista->numeroElementos = 0;
 	return 0;
 }
 
@@ -18,7 +22,16 @@ extern ElementoLista *Lista_Buscar(ListaEnlazada *lista, void *objeto){
 
 /*Funciones de modificacion de la lista*/
 extern int Lista_InsertarFin(ListaEnlazada *lista, void *objeto){
-	return 0;
+	ElementoLista *nuevo_elemento;
+	if ((nuevo_elemento = alloc (nuevo_elemento)) == NULL)
+		return -1;
+	strcpy (nuevo_elemento->objeto, objeto);
+	nuevo_elemento->siguiente = NULL;
+	nuevo_elemento->anterior = lista->fin;
+	lista->fin->siguiente = nuevo_elemento;
+	lista->fin = nuevo_elemento;
+	lista->tamaño++;
+  return 0;
 }
 extern int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto){
 	return 0;
