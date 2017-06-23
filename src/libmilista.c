6adc4f5e3f95d63afeff0d0216692e69f972ef57
root <diegorobalino16@gmail.com>
2017-06-23 02:04:38 -0500

    [comentando]

--- src/libmilista.c
+++ src/libmilista.c
@@ -3,21 +3,21 @@
 /*Definicion de funciones sobre la lista*/
 
 extern int Lista_Inicializar(ListaEnlazada *lista){
-	lista->ancla.objeto = NULL;
-	lista->ancla.siguiente = NULL;
-	lista->ancla.anterior = NULL;
-	lista->numeroElementos = 0;
+	lista->ancla.objeto = NULL;                    //inicializamos el apuntador del ancla
+	lista->ancla.siguiente = NULL;			//inicializamos el apuntador a siguiente
+	lista->ancla.anterior = NULL;			//inicializamos el apuntador a anterior
+	lista->numeroElementos = 0;			//inicialmente la lista esta vacia
 	return 1;
 }
 
 extern int Lista_Vacia(ListaEnlazada *lista){
 	//printf("esta vacia\n");
-	for(ElementoLista *it = lista->ancla.siguiente; it->objeto != NULL; it = it->siguiente){
-		free(it);
-		lista->numeroElementos--;
+	for(ElementoLista *it = lista->ancla.siguiente; it->objeto != NULL; it = it->siguiente){  //iteramos dentro de la lista con la variable it
+		free(it);				//liberamos la memoria de cada elemento
+		lista->numeroElementos--;		//reducimos los numeros de elemntos de la lista
 	}
-	if (lista->numeroElementos == 0){
-		lista->ancla.siguiente = NULL;
+	if (lista->numeroElementos == 0){		//si el numero de elemetos dentro de la lista es 0
+		lista->ancla.siguiente = NULL;		//reseteamos los apuntadores del ancla
 		lista->ancla.anterior = NULL;
 
 		return 1;
@@ -26,14 +26,14 @@ extern int Lista_Vacia(ListaEnlazada *lista){
 	return 0;
 }
 extern int Lista_Conteo(ListaEnlazada *lista){
-	return (int)lista->numeroElementos;
+	return (int)lista->numeroElementos;			//retorna el valor del numero de elemntos
 }
 
 extern ElementoLista *Lista_Buscar(ListaEnlazada *lista, void *objeto){
-	ElementoLista *actual = (ElementoLista*)malloc(sizeof(ElementoLista));
+	ElementoLista *actual = (ElementoLista*)malloc(sizeof(ElementoLista)); //se crea un nuevo ElementoLista y se asigna memoria
 	*actual = lista->ancla;
-	for (int i = 0; i <= lista->numeroElementos; i++){
-		if (actual->objeto == objeto){
+	for (int i = 0; i <= lista->numeroElementos; i++){			//se itera sobre la lista
+		if (actual->objeto == objeto){					//se compara el elemnto de la lista con el elemento introducido para buscar
 			return actual;
 		}
 		
@@ -44,26 +44,26 @@ extern ElementoLista *Lista_Buscar(ListaEnlazada *lista, void *objeto){
 
 /*Funciones de modificacion de la lista*/
 extern int Lista_InsertarFin(ListaEnlazada *lista, void *objeto){
-	ElementoLista *nuevo_elemento = (ElementoLista*)malloc(sizeof(ElementoLista));
+	ElementoLista *nuevo_elemento = (ElementoLista*)malloc(sizeof(ElementoLista)); //se crea un nuevo elemento y se asigna memoria
 	nuevo_elemento->objeto = objeto;
 	//printf("%d\n", nuevo_elemento->objeto);
 
-	if (lista->numeroElementos == 0){
-		nuevo_elemento->siguiente = &lista->ancla;
-		nuevo_elemento->anterior = &lista->ancla;
-		lista->ancla.siguiente = nuevo_elemento;
-		lista->ancla.anterior = nuevo_elemento;
-		lista->numeroElementos++;
-	} else{
-		ElementoLista *actual = (ElementoLista*)malloc(sizeof(ElementoLista));
+	if (lista->numeroElementos == 0){			//si la lista esta vacia
+		nuevo_elemento->siguiente = &lista->ancla;	//se le asigna la posisicion del ancla ya que sera el primer elemnto de la lista
+		nuevo_elemento->anterior = &lista->ancla;	//como es el primer elemento este estara rodeado por el ancla
+		lista->ancla.siguiente = nuevo_elemento;	//el apuntador del ancla.siguiente sera al nuevo elemento
+		lista->ancla.anterior = nuevo_elemento;		//el apuntador del ancla.anterior sera el nuevo elemento ya que sera el primer elemento
+		lista->numeroElementos++;			//aumento el numero de elementos de la lista
+	} else{							//caso contrario
+		ElementoLista *actual = (ElementoLista*)malloc(sizeof(ElementoLista)); //creo el elemento actual y asigno memoria
 		*actual = lista->ancla;
-		for (int i = 0; i <= lista->numeroElementos; i++){
+		for (int i = 0; i <= lista->numeroElementos; i++){			//recorremos la lista segun el numero de elemntos
 			if (actual->siguiente == &lista->ancla){
 				actual->siguiente = nuevo_elemento;
 				nuevo_elemento->anterior = actual;
-				nuevo_elemento->siguiente = &lista->ancla;
-				lista->ancla.anterior = nuevo_elemento;
-				lista->numeroElementos++;
+				nuevo_elemento->siguiente = &lista->ancla;		//el apuntador siguiente de nuevo elemento  debe ubicar a la posicion del ancla
+				lista->ancla.anterior = nuevo_elemento;			//el apuntador del ancla anterior debera ser el nuevo elemnto ya que se lo agrega al final
+				lista->numeroElementos++;				//aumento el numero de los elemntos de la lista
 				//printf("%d\n", i);
 				return 1;
 			}
@@ -74,8 +74,8 @@ extern int Lista_InsertarFin(ListaEnlazada *lista, void *objeto){
 	
   	return 0;
 }
-extern int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto){
-	ElementoLista *nuevo_elemento = (ElementoLista*)malloc(sizeof(ElementoLista));
+extern int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto){                      //se realiza lo mismo de la funcion anterior solo que ahora al principio
+	ElementoLista *nuevo_elemento = (ElementoLista*)malloc(sizeof(ElementoLista));	 
 	nuevo_elemento->objeto = objeto;
 	//strcpy(nuevo_elemento->objeto, objeto);
 	if (lista->numeroElementos == 0){
@@ -95,23 +95,23 @@ extern int Lista_InsertarInicio(ListaEnlazada *lista, void *objeto){
 	}  	return 0;
 }
 extern void Lista_Sacar(ListaEnlazada *lista, ElementoLista *elemento){
-	elemento->anterior->siguiente = elemento->siguiente;
+	elemento->anterior->siguiente = elemento->siguiente;			//se re-asignan los apuntadores del elemento a eliminar
 	elemento->siguiente->anterior = elemento->anterior;
-	lista->numeroElementos--;
+	lista->numeroElementos--;						//se reduce el numero de elemntos de la lista
 }
-extern void Lista_SacarTodos(ListaEnlazada *lista){
+extern void Lista_SacarTodos(ListaEnlazada *lista){				//vaciara la lista
 	//printf("%d\n", lista->numeroElementos);
-	ElementoLista *actual = (ElementoLista*)malloc(sizeof(ElementoLista));
-	ElementoLista *next = (ElementoLista*)malloc(sizeof(ElementoLista));
-	actual = lista->ancla.siguiente;
-	next = actual->siguiente;
-	while (actual != &lista->ancla){
-		free(actual);
-		lista->numeroElementos--;
-		actual = (ElementoLista*)malloc(sizeof(ElementoLista));
+	ElementoLista *actual = (ElementoLista*)malloc(sizeof(ElementoLista));   //se cre el elemnto actual y se asigna memoria
+	ElementoLista *next = (ElementoLista*)malloc(sizeof(ElementoLista));	//se crea el elemento next y se asigna memoria
+	actual = lista->ancla.siguiente;					//se inicializa actual con el primer valor de la lista
+	next = actual->siguiente;						//se inicializa con  el siguiente valor de actual
+	while (actual != &lista->ancla){					//mientras actual sea diferente de la posicion del ancla
+		free(actual);							//liberamos mememoria de actual
+		lista->numeroElementos--;					//reducimos el numero de elementos de la lista
+		actual = (ElementoLista*)malloc(sizeof(ElementoLista));		
 		actual = next;
-		if (actual == &lista->ancla){
-			return;
+		if (actual == &lista->ancla){					//si actual es igual a la posicion del ancla 
+			return;							//no hace nada y sale
 		}
 		
 		next = next->siguiente;
